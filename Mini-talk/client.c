28eb5fc23d65b7cf5fc9f5a3420f7185b05e47c8
Kvjhaynes <88121864+Kvjhaynes@users.noreply.github.com>
2022-04-13 13:40:45 +0930

    Add files via upload

---
+++ Mini-talk/client.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khaynes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 10:46:13 by khaynes           #+#    #+#             */
+/*   Updated: 2022/04/05 12:29:29 by khaynes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include "./ft_printf/ft_printf.h"
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	ft_atoi(const char *str)
+{
+	long			res;
+	long			sign;
+	unsigned int	i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\f'
+		|| str[i] == '\r' || str[i] == '\v')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return ((int)(res * sign));
+}
+
+void	send_bit(int pid, char *str, size_t len)
+{
+	size_t	i;
+	int		j;
+
+	i = 0;
+	while (i <= len)
+	{
+		j = 0;
+		while (j < 7)
+		{
+			if (str[i] & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			str[i] >>= 1;
+			j++;
+			usleep(300);
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+	char	*str;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		str = argv[2];
+		send_bit(pid, str, ft_strlen(str));
+	}
+	else
+		ft_printf("\n INVALID PARAMATERS! \n");
+}
---
+++ Mini-talk/server.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khaynes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 10:58:10 by khaynes           #+#    #+#             */
+/*   Updated: 2022/04/06 12:09:25 by khaynes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include "./ft_printf/ft_printf.h"
+
+typedef struct s_message
+{
+	char	c;
+	size_t	i;
+}	t_message;
+
+t_message	g_msg;
+
+void	bit_handler(int bit)
+{
+	g_msg.c += ((bit & 1) << g_msg.i);
+	g_msg.i++;
+	if (g_msg.i == 7)
+	{
+		ft_printf("%c", g_msg.c);
+		if (!g_msg.c)
+			ft_printf("\n");
+		g_msg.c = 0;
+		g_msg.i = 0;
+	}
+}
+
+int	main(void)
+{
+	ft_printf("[SERVER SUCCESS]\n");
+	ft_printf("[PID: %d]\n", getpid());
+	while (1)
+	{
+		signal(SIGUSR2, bit_handler);
+		signal(SIGUSR1, bit_handler);
+		pause();
+	}
+	return (0);
+}
